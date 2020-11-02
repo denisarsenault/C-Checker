50ce478a2e8d7a88703a9e106de3cdf4a5dda4ab
Denis Arsenault <denis2@me.com>
2020-11-02 15:39:03 -0500

    -- Baseline commit

--- C++Checker/main.cpp
+++ C++Checker/main.cpp
@@ -10,5 +10,14 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    
+#if __cplusplus == 201402L
+   std::cout << "C++14" << std::endl;
+ #elif __cplusplus==201103L
+   std::cout << "C++11" << std::endl;
+ #else
+   std::cout << "c/c++" << std::endl;
+ #endif
+ return 0;
     return 0;
 }
